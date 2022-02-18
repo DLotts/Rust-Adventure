0664a4f64193cf6a8199670c9faa717862faa516
David Lotts <david@lotts.org>
2022-02-18 01:15:05 -0500

    inventory command and more cave and  paths.

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,6 @@ use std::{
 };
 
 use lazy_static::lazy_static; // 1.4.0
-
-#[derive(PartialEq, Eq)]
-enum Item {
-    Light(),
-    Axe(),
-}
-
-struct Inventory {
-    bag: Mutex<Vec<Item>>,
-}
-
-impl Inventory {
-    pub fn new() -> Inventory {
-        Inventory {
-            bag: Mutex::new(Vec::<Item>::new()),
-        }
-    }
-    pub fn add(&self, item: Item) {
-        self.bag.lock().unwrap().push(item)
-    }
-    pub fn contains(&self, item: Item) -> bool {
-        self.bag.lock().unwrap().contains(&item)
-    }
-}
 lazy_static! {
     static ref INV: Inventory = Inventory::new();
 }
@@ -104,37 +80,114 @@ fn cave_entry() {
     }
 }
 fn east_cave_path() {
-    println!("Dead end.  Going back.")
+    loop {
+        println!(
+            "What is that?  Hold your light up to the ceiling.
+        oh my, bats!  Shhhhh, don't wake them up!
+        I hear growling to the west.
+        dark passage to the north and west."
+        );
+        let words = get_words();
+        match words[0].as_str() {
+            "north" => west_cave_path(),
+            "west" => cave_entry(),
+            _ => default(words),
+        }
+    }
 }
 
 fn west_cave_path() {
-    println!("Dead end.  Going back.")
+    loop {
+        println!(
+            "Hmmm, the floor is muddy, and a little sticky, and stinky.
+        Stinky? Wait, that's not mud, that's bear poo!
+        I hear growling to the east.
+        dark passage to the north and east."
+        );
+        let words = get_words();
+        match words[0].as_str() {
+            "north" => east_cave_path(),
+            "east" => cave_entry(),
+            _ => default(words),
+        }
+    }
 }
 
 fn east_path() {
-    println!(
-        "On a shady path.
+    loop {
+        println!(
+            "On a shady path.
     Rocks up ahead to the west.
     Is that a house to  the east?"
-    );
-    //if !INV.lock().unwrap().contains(&Item::Light()) {
-    if !INV.contains(Item::Light()) {
-        println!("Before you get very far, You find a nice little LED flashlight!");
-        //    INV.lock().unwrap().push(Item::Light());
-        INV.add(Item::Light());
-    } else {
-        println!("Too boring, heading back!");
+        );
+        if !INV.contains(Item::Light()) {
+            println!("You found a powerful LED flashlight! Might need this!");
+            INV.add(Item::Light());
+        } else {
+            println!("This is where I found my flashlight.");
+        }
+        let words = get_words();
+        match words[0].as_str() {
+            "east" => println!("Aaaa!, there is a cliff from erosion!  Can't go this way."),
+            "west" => outside_cave(),
+            _ => default(words),
+        }
     }
 }
 fn dense_forrest() {
     println!("Terribly dense forest -- can't get anywhere.")
 }
 fn west_path() {
-    if !INV.contains(Item::Axe()) {
-        println!("Oh look, an Axe!");
-        INV.add(Item::Axe());
+    loop {
+        if !INV.contains(Item::Axe()) {
+            println!("Oh look, a rusty axe!");
+            INV.add(Item::Axe());
+        }
+        println!(
+            "Beutiful flowers grow all around a rusty spot where an axe was.
+            Dense forrest overtakes the path to the west.
+            Path continues to the east."
+        );
+        let words = get_words();
+        match words[0].as_str() {
+            "west" => dense_forrest(),
+            "east" => outside_cave(),
+            _ => default(words),
+        }
     }
-    println!("You get bored and head back toward the cave.");
+}
+
+#[derive(PartialEq, Eq, strum_macros::Display)]
+enum Item {
+    Light(),
+    Axe(),
+}
+
+struct Inventory {
+    bag: Mutex<Vec<Item>>,
+}
+
+impl Inventory {
+    pub fn new() -> Inventory {
+        Inventory {
+            bag: Mutex::new(Vec::<Item>::new()),
+        }
+    }
+    pub fn add(&self, item: Item) {
+        self.bag.lock().unwrap().push(item)
+    }
+    pub fn contains(&self, item: Item) -> bool {
+        self.bag.lock().unwrap().contains(&item)
+    }
+    fn list(&self) -> String {
+        self.bag
+            .lock()
+            .unwrap()
+            .iter()
+            .map(Item::to_string)
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
 }
 
 fn get_words() -> Vec<String> {
@@ -144,7 +197,8 @@ fn get_words() -> Vec<String> {
         let the_line: String = read!("{}\n");
         let words: Vec<String> = the_line.split_whitespace().map(|s| s.to_string()).collect();
         match &words[..] {
-            [verb,..] if verb=="help" => println!("Try east, west, north, or south.  Or maybe a two word sentence: verb noun, like 'eat bread'."),
+            [verb,..] if verb=="help" => println!("Try east, west, north, south, or inventory.  Or maybe a two word sentence: verb noun, like 'eat bread'."),
+            [verb,..] if verb=="inventory" => println!("inventory:\n{}",INV.list()),
             _ => {println!("============"); return words}
         }
     }
